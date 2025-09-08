d90e31e99ca47a6beed81e056a9103a4cec38477
Doom <a_alrajhi@rocketmail.com>
2025-09-08 10:35:57 +0300

    Project initialize

---
+++ Backend/src/main/java/com/event_website/EventWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.event_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventWebsiteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EventWebsiteApplication.class, args);
+  }
+}

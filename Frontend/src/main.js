2814d7584c79b85aecc0ac7f2bacaa078476a39a
SWE-Yazeed <yazeed.alhariqi@gmail.com>
2025-09-13 14:05:49 +0300

    save changes

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from './Pages/Login.vue';
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: Login },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(router).mount('#app');
